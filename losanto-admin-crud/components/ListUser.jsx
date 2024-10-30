21f52714a046845f8f6b096bdbe93998d28f1c5b
AxlDevLinux <axesan917@gmail.com>
2024-10-30 11:38:30 -0600

    tabla list user conectada a la base de datos, actions buttons

--- losanto-admin-crud/components/ListUser.jsx
+++ losanto-admin-crud/components/ListUser.jsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from "react";
-import Link from "next/link";
+"use client"
+
+
+import axios from 'axios'
+import { useEffect, useState } from "react"
+import Link from "next/link"
 
 export default function ListUser() {
-    //const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         getUsers()
     }, [])
 
-    function getUsers(){
-        axios.get('http://localhost:3001/api/users').then(function(response){
-            console.log(response)
+    function getUsers() {
+        axios.get('http://localhost:3001/api/users').then(function (response) {
+            setUsers(response.data)
         })
     }
 
-    return(
+    return (
         <table className="table table-zebra">
             <thead className="text-sm text-gray-700 uppercase bg-gray-50">
                 <tr>
@@ -23,26 +27,30 @@ export default function ListUser() {
                     <th className="py-3 px-6">Email</th>
                     <th className="py-3 px-6">Username</th>
                     <th className="py-3 px-6 text-center">Actions</th>
-                                        
+
                 </tr>
             </thead>
             <tbody>
-                <tr className="bg-white border-b">
-                    <td className="py-3 px-6  bg-blue-700">1</td>
-                    <td className="py-3 px-6 bg-blue-700">Axel sandoval</td>
-                    <td className="py-3 px-6 bg-blue-700">asandoval13@ucol.mx</td>
-                    <td className="py-3 px-6 bg-blue-700">Suzuya</td>
-                    <td className="flex justify-center gap-1 py-3">
-                        <Link
-                            href="#"
-                            className="btn btn-success">
-                            Read
-                        </Link>
-                        <Link className="btn btn-info" href="#">
-                        Edit</Link>
-                        <button className="btn btn-error">Delete</button>
-                    </td>
-                </tr>
+                {users.map((user, key) =>
+                    <tr key={key} className="bg-white border-b">
+                        <td className="py-3 px-6">{user.id}</td>
+                        <td className="py-3 px-6">{user.name}</td>
+                        <td className="py-3 px-6">{user.email}</td>
+                        <td className="py-3 px-6">{user.username}</td>
+                        <td className="flex justify-center gap-1 py-3">
+                            <Link
+                                href="#"
+                                className="btn btn-success">
+                                Read
+                            </Link>
+                            <Link className="btn btn-info" href="#">
+                                Edit
+                            </Link>
+                            <button className="btn btn-error">Delete</button>
+                        </td>
+                    </tr>
+                )}
+
             </tbody>
         </table>
     )
